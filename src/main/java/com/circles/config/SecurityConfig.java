fece5c36e43078f9ff85934399718c554d2b050d
오한별 <suriessay@naver.com>
2020-04-23 23:01:31 +0900

    SecurityConfig 예외 url 설정

--- src/main/java/com/circles/config/SecurityConfig.java
+++ src/main/java/com/circles/config/SecurityConfig.java
@@ -1,4 +1,16 @@
 package com.circles.config;
 
-public class SecurityConfig {
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/","/login","/sign-up").permitAll()
+                .mvcMatchers(HttpMethod.GET, "profile/*").permitAll()
+                .anyRequest().authenticated();
+    }
 }
