45e36cc5cf25ce85dadcff591334caa358ad6207
오한별 <suriessay@naver.com>
2020-04-27 23:49:44 +0900

    valid check modelattribute 작성

--- src/main/java/com/circles/account/AccountController.java
+++ src/main/java/com/circles/account/AccountController.java
@@ -2,13 +2,28 @@ package com.circles.account;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AccountController {
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model){
+        model.addAttribute("signUpForm", new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("sign-up")
+    public String signUpSubmit(@Valid @ModelAttribute SignUpForm signUpForm, Errors errors){ //파라미터로 쓰일때 @ModelAttribute 생략가능
+        if(errors.hasErrors()){
+            return "account/sign-up";
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/circles/account/SignUpForm.java
+++ src/main/java/com/circles/account/SignUpForm.java
@@ -1,4 +1,24 @@
 package com.circles.account;
 
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Data
 public class SignUpForm {
+    @NotBlank
+    @Length(min=4, max=15)
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{4,15}$")
+    private String nickname;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Length(min=6, max=20)
+    private String password;
 }
