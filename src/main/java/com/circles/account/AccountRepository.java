29d7816bc367d87b4feb758f58e18c8dc35917b5
오한별 <suriessay@naver.com>
2020-04-28 23:24:07 +0900

    validator 작성

---
+++ src/main/java/com/circles/account/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.circles.account;
+
+import com.circles.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
