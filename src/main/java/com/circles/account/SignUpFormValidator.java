03cff71d57169c3eba6214a68a0eb953d7ab1d8c
오한별 <suriessay@naver.com>
2020-04-29 22:43:20 +0900

    validator commit

--- src/main/java/com/circles/account/SignUpFormValidator.java
+++ src/main/java/com/circles/account/SignUpFormValidator.java
@@ -16,15 +16,15 @@ public class SignUpFormValidator implements Validator {
         return aClass.isAssignableFrom(SignUpForm.class);
     }
 
-    @Override
-    public void validate(Object o, Errors errors) {
-        SignUpForm signUpForm = (SignUpForm) errors;
-        if(accountRepository.existsByEmail(signUpForm.getEmail())){
-            errors.rejectValue("email", "invalid.email", new Object[]{signUpForm.getEmail()}, "사용중인 이메일 입니다");
-        }
+        @Override
+        public void validate(Object o, Errors errors) {
+            SignUpForm signUpForm = (SignUpForm) errors;
+            if(accountRepository.existsByEmail(signUpForm.getEmail())){
+                errors.rejectValue("email", "invalid.email", new Object[]{signUpForm.getEmail()}, "사용중인 이메일 입니다");
+            }
 
-        if(accountRepository.existsByNickname(signUpForm.getNickname())){
-            errors.rejectValue("nickname", "invalid.nickname", new Object[]{signUpForm.getNickname()}, "사용중인 닉네임 입니다");
-        }
+            if(accountRepository.existsByNickname(signUpForm.getNickname())){
+                errors.rejectValue("nickname", "invalid.nickname", new Object[]{signUpForm.getNickname()}, "사용중인 닉네임 입니다");
+            }
     }
 }
