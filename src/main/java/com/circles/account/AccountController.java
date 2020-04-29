dd8f560600ea387da902863ba4750d204a2634ec
오한별 <suriessay@naver.com>
2020-04-29 23:00:22 +0900

    singup 작성, javamailsender 작성, profile설정

--- src/main/java/com/circles/account/AccountController.java
+++ src/main/java/com/circles/account/AccountController.java
@@ -1,16 +1,28 @@
 package com.circles.account;
 
+import com.circles.domain.Account;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+    private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+
+    @InitBinder("signUpFrom")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model){
@@ -20,9 +32,22 @@ public class AccountController {
 
     @PostMapping("sign-up")
     public String signUpSubmit(@Valid @ModelAttribute SignUpForm signUpForm, Errors errors){ //파라미터로 쓰일때 @ModelAttribute 생략가능
+        //signUpFormValidator.validate(signUpForm, errors);
         if(errors.hasErrors()){
             return "account/sign-up";
         }
+
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword())
+                .emailVerified(false)
+                .circleCreatedByEmail(true)
+                .circleCreatedByWeb(true)
+                .circleUpdatedResultByWeb(true)
+                .build();
+
+        Account saveAccount = accountRepository.save(account);
 
         return "redirect:/";
     }
