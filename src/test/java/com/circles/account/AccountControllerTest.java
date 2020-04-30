0e215ab252096d989b909961326d61336325374b
오한별 <suriessay@naver.com>
2020-04-30 21:59:16 +0900

    회원가입시 입력값에 따른 테스트 작성

--- src/test/java/com/circles/account/AccountControllerTest.java
+++ src/test/java/com/circles/account/AccountControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -19,6 +21,9 @@ public class AccountControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
     @DisplayName("회원 가입 화면 노출 여부 테스트")
     @Test
     public void signUpForm() throws Exception {
@@ -28,4 +33,33 @@ public class AccountControllerTest {
                 .andExpect(view().name("account/sign-up"))
                 .andExpect(model().attributeExists("signUpForm"));
     }
+
+    @DisplayName("회원 가입 버튼클릭시 - 잘못된 입력값")
+    @Test
+    public void 회원가입버튼클릭시잘못된입력값() throws Exception{
+        mockMvc.perform(post("/sign-up")
+                .param("nickname","nick")
+                .param("email","isnotemailform")
+                .param("password","12345")
+                .with(csrf()))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("account/sign-up"));
+
+    }
+
+    @DisplayName("회원 가입 버튼클릭시 - 정확한 입력값")
+    @Test
+    public void 회원가입버튼클릭시정확한입력값() throws Exception{
+        mockMvc.perform(post("/sign-up")
+                .param("nickname","nickname")
+                .param("email","isemail@gmail.com")
+                .param("password","123456")
+                .with(csrf()))
+                    .andExpect(status().is3xxRedirection())
+                    .andExpect(view().name("redirect:/"));
+
+        assertTrue(accountRepository.existsByEmail("isemail@gmail.com"));
+
+    }
+
 }
