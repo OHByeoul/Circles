26d1cf0a7a98f0700306dec49462288bcf044d47
오한별 <suriessay@naver.com>
2020-07-06 19:36:20 +0900

    get resource from csv and save the datas

---
+++ src/main/java/com/circles/zone/ZoneService.java
@@ -0,0 +1,37 @@
+package com.circles.zone;
+
+import com.circles.domain.Zone;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ZoneService {
+        private final ZoneReopsitory zoneReopsitory;
+
+        @PostConstruct
+        public void initZoneData() throws IOException {
+            if(zoneReopsitory.count() == 0){
+                Resource resource = new ClassPathResource("cityOfKorea.csv");
+               List<Zone> excelLists = Files.readAllLines(resource.getFile().toPath(), StandardCharsets.UTF_8).stream()
+                        .map(each -> {
+                            String [] split = each.split(",");
+                            return Zone.builder().city(split[0]).localNameOfCity(split[1]).province(split[2]).build();
+                        }).collect(Collectors.toList());
+
+                zoneReopsitory.saveAll(excelLists);
+            }
+        }
+}
